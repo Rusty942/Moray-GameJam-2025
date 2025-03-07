42e45fdb7fa31bd7b75ec8b84a92041a3c374c13
Kyle Jones <56350720+Rusty942@users.noreply.github.com>
2025-03-07 15:56:41 +0000

    Finished fuel logic
    Fuel logic made

--- MorayGameJam25/Assets/Scripts/ShipFuel.cs
+++ MorayGameJam25/Assets/Scripts/ShipFuel.cs
@@ -8,10 +8,28 @@ public class ShipFuel : MonoBehaviour
     public int maxFuel = 500;
     public int currentFuel;
     public FuelBar fuelBar;
+    
+    [SerializeField] private int idleFuelConsumption = 1;
+    [SerializeField] private int movingFuelConsumption = 2;
+    
+    private Rigidbody2D rb;
 
     private void Start()
     {
         currentFuel = maxFuel;
         fuelBar.setMaxFuel(maxFuel);
+        rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(ConsumeFuel());
+    }
+    
+    private IEnumerator ConsumeFuel()
+    {
+        while (currentFuel > 0)
+        {
+            int fuelRate = rb.velocity.magnitude > 0 ? movingFuelConsumption : idleFuelConsumption;
+            currentFuel = Mathf.Max(currentFuel - fuelRate, 0);
+            fuelBar.setFuel(currentFuel);
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
