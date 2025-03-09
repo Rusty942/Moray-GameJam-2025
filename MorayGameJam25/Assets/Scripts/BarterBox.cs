a1749d95bf1ee3a808da8f745ccf1913d7aada59
Kylejones01340 <56350720+Rusty942@users.noreply.github.com>
2025-03-09 04:28:41 +0000

    Finished barter system

--- MorayGameJam25/Assets/Scripts/BarterBox.cs
+++ MorayGameJam25/Assets/Scripts/BarterBox.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +8,21 @@ public class BarterBox : MonoBehaviour
     public float slideDuration = 1.0f;
     public Vector3 targetPosition;
     public bool onScreen = false;
+    public GameObject fill1;
+    public GameObject fill2;
+    public int item1Value;
+    public int item2Value;
+    public TextMeshPro item1ammvis;
+    public TextMeshPro item2ammvis;
+    private bool item1 = true;
+
+    private void Start()
+    {
+        fill1.SetActive(true);
+        fill2.SetActive(false);
+        item1ammvis.text = (item1Value.ToString());
+        item2ammvis.text = (item2Value.ToString());
+    }
 
     void Update()
     {
@@ -14,6 +30,48 @@ public class BarterBox : MonoBehaviour
         {
             SceneManager.LoadScene("Space");
         }
+        float verticalInput = Input.GetAxis("Vertical");
+        if (verticalInput < 0 && item1)
+        {
+            item1 = false;
+            fill1.SetActive(false);
+            fill2.SetActive(true);
+        }
+        else if (verticalInput > 0 && !item1)
+        {
+            item1 = true;
+            fill1.SetActive(true);
+            fill2.SetActive(false);
+        }
+
+        if (Input.GetButtonDown("Jump")){
+            if (item1)
+            {
+                int minus = StartData.currencyAmm - item1Value;
+                if ( minus < 0)
+                {
+                    
+                }
+                else
+                {
+                    StartData.currencyAmm -= item1Value;
+                }
+            }
+            else
+            {
+                int minus = StartData.currencyAmm - item2Value;
+                if (minus < 0)
+                {
+
+                }
+                else
+                {
+                    StartData.currencyAmm -= item2Value;
+                }
+
+            }
+        }
+
     }
 
     public IEnumerator SlideInBarterBox()
